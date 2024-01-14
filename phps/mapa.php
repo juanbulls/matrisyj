3042c96a6abfae635b5f33a03f3cc8c87425b2a5
ne-jcruiz <juanbulls@yahoo.com>
2024-01-14 18:27:48 -0500

    Update mapa.php

--- phps/mapa.php
+++ phps/mapa.php
@@ -2,15 +2,15 @@
 $clave = isset($_REQUEST["clave"]) ? $_REQUEST["clave"] : (isset($argv[1]) ? $argv[1] : null);
 $permitidas = array("nana", "mona", "prado", "sumo", "prado sumo");
 
-$secMapa = " <p> <b>Indicaciones</b> </p> <p>" +
-+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-+ "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-+ "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
-+ "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
-+ "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-+ "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-+ "culpa qui officia deserunt mollit anim id est laborum."
-+ "</p> <img src='mapaDummy.png'>";
+$secMapa = " <p> <b>Indicaciones</b> </p> <p> \
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
+    culpa qui officia deserunt mollit anim id est laborum.\
+    </p> <img src='mapaDummy.png'>";
 
 if (in_array(strtolower($clave), $permitidas)) {
     echo $secMapa;
