93e291b6897b9e3aa86b95856740d2cf21be1ace
ne-jcruiz <juanbulls@yahoo.com>
2024-01-15 22:03:45 -0500

    Reemplazo mapa dummy y vestimentas

--- phps/mapa.php
+++ phps/mapa.php
@@ -2,15 +2,24 @@
 $clave = isset($_REQUEST["clave"]) ? $_REQUEST["clave"] : (isset($argv[1]) ? $argv[1] : null);
 $permitidas = array("nana", "mona", "prado", "sumo", "prado sumo", "toyota prado");
 
-$secMapa = " <p> <b>Indicaciones</b> </p> <p> \
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
-    culpa qui officia deserunt mollit anim id est laborum.\
-    </p> <img src='mpim/mapaDummy.png'>";
+$secMapa = " <p> <b>Indicaciones</b> </p> <p>"
+. "<br><b>Por la Calera</b> <ul>"
+. "<li>Luego del peaje de patios </li>"
+. "<li>Pasando la pradera de potosi </li>"
+. "<li>Antes de la rotonda que desvia a Guasca, a mano derecha es la misa en la capilla de la hacienda La Selva </li>"
+. "<li>Para la recepción siga manejando hacia Sopó </li>"
+. "<li>Luego de la rotonda que desvia a Guasca y antes de Sopo busque el restaurante XXX a mano derecha</li>"
+. "<li>Desviese a mano derecha, entre el restaurante y la cancha de la escuelita maneje derecho hasta el porton de brujas</li>"
+. "<li>Ya llegó, parquie frente a alguien si quiere irse temprano, si hay espacio y se quiere ir tarde, pegado al muro del este</li></ul>"
+. "<br><b>Por la Sopó</b> <ul>"
+. "<li>Luego del peaje de la autopista </li>"
+. "<li>Desviese hacia Sopó a mano derecha </li>"
+. "<li>En la rotonda de la Cabaña tome la primera salida hacia el sur </li>"
+. "<li>Despues de Castilac </li>"
+. "<li>Desviese a mano izquierda por entre la cancha de la escuelita y el restaurante XXX</li>"
+. "<li>Maneje derecho hasta el porton de brujas</li>"
+. "<li>Ya llegó, parquie frente a alguien si quiere irse temprano, si hay espacio y se quiere ir tarde, pegado al muro del este</li></ul>"
+. "</ul> <img src='mpim/mapa.jpg'>";
 
 if (in_array(strtolower($clave), $permitidas)) {
     echo $secMapa;
