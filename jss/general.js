78447a8dd66778b0ebe8c3d1459a851d4186765e
ne-jcruiz <juanbulls@yahoo.com>
2024-01-18 00:31:32 -0500

    Expander js 2

--- jss/general.js
+++ jss/general.js
@@ -56,8 +56,10 @@ async function enviarAsistentes(asistentes){
 
 function expander(expandido){
     if (expandido) {
-        
+        id("imgMapa").className = "imgMapa";
+        id("imgMapa").value = false;
     } else {
-
+        id("imgMapa").className = "imgMapote";
+        id("imgMapa").value = true;
     }
 }
