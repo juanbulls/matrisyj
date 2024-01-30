a9a1e30a5f81be9849978a698f88a6369601e460
ne-jcruiz <juanbulls@yahoo.com>
2024-01-30 12:13:43 -0500

    Indicaciones y orden pistas

--- phps/mapa.php
+++ phps/mapa.php
@@ -22,21 +22,22 @@ $secMapa = "<button class='botonEmail'"
 . "<br>"
 . "<b>Por la Calera</b> <ul>"
 . "<li>Luego del peaje de patios </li>"
-. "<li>Pasando la pradera de potosi </li>"
-. "<li>Antes de la rotonda que desvia a Guasca, a mano derecha es la misa en la capilla de la hacienda La Selva </li>"
+. "<li>Pasando la pradera de Potosí </li>"
+. "<li>Antes de la rotonda que desvía a Guasca a mano derecha es la misa. En la capilla de la hacienda La Selva </li>"
 . "<li>Para la recepción siga manejando hacia Sopó </li>"
-. "<li>Luego de la rotonda que desvia a Guasca y antes de Sopo busque el restaurante XXX a mano derecha</li>"
-. "<li>Desviese a mano derecha, entre el restaurante y la cancha de la escuelita maneje derecho hasta el porton de brujas</li>"
-. "<li>Ya llegó, parquie frente a alguien si quiere irse temprano, si hay espacio y se quiere ir tarde, pegado al muro del este</li></ul>"
+. "<li>Luego de la rotonda que desvía a Guasca y antes del pueblo de Sopo, busque el restaurante My Food a mano derecha</li>"
+. "<li>Salga de la carretera manejando por entre el restaurante y la cancha de la escuelita, maneje derecho hasta el portón de brujas</li>"
+. "<li>Ya llegó, parquee frente a alguien si quiere irse temprano, si planea quedarse hasta tarde, trate de parquear sin bloquear a los que salen temprano, para eso parquee contra el muro norte.li></ul>"
 . "<br>"
 . "<b>Por Sopó</b> <ul>"
 . "<li>Luego del peaje de la autopista </li>"
-. "<li>Desviese hacia Sopó a mano derecha </li>"
+. "<li>Desvíese hacia Sopó a mano derecha </li>"
 . "<li>En la rotonda de la Cabaña tome la primera salida hacia el sur </li>"
-. "<li>Despues de Castilac </li>"
-. "<li>Desviese a mano izquierda por entre la cancha de la escuelita y el restaurante XXX</li>"
-. "<li>Maneje derecho hasta el porton de brujas</li>"
-. "<li>Ya llegó, parquie frente a alguien si quiere irse temprano, si hay espacio y se quiere ir tarde, pegado al muro del este</li></ul>";
+. "<li>Despues de Castilac maneje hasta el restaurante My Food </li>"
+. "<li> Salga de la carretera manejando por entre el restaurante y la cancha de la escuelita, maneje derecho hasta el portón de brujas </li>"
+. "<li>Maneje derecho hasta el portón de brujas</li>"
+. "<li> Ya llegó, parquee frente a alguien si quiere irse temprano, si planea quedarse hasta tarde, trate de parquear sin bloquear a los que salen temprano, para eso parquee contra el muro norte.</li></ul>";
+
 
 
 if (in_array(strtolower($clave), $permitidas)) {
