eb32569a7c3a8b9969f6b2173735b15ad6dcb72c
ne-jcruiz <juanbulls@yahoo.com>
2024-02-07 17:36:49 -0500

    Animacion y efecto balanceo en desviacion x

--- jss/ramas.js
+++ jss/ramas.js
@@ -21,23 +21,26 @@ ct2.shadowOffsetX = 2;
 ct2.shadowOffsetY = 2;
 
 function lineaCapullo(l, x, y, bx, by, xf, yf){
+    /* Juan Wills animation, see other canvas efects
+    on jcwills.com ⛵ */
     l.moveTo(x, y);
     l.quadraticCurveTo(bx, by, xf, yf);
 }
+var desvX = (Math.random() * 20) - 10;
+var desv2X = (Math.random() * 10) - 5;
 function flor(l, x, y){
     var altura = 20;
-    //l.strokeStyle = 'rgba(250 10 10)'; //quitar
-    var desvX = (Math.random() * 20) - 10;
     l.beginPath();
     lineaCapullo(l, x, y, x-altura/2, y-altura/2, x+desvX, y-altura);
     lineaCapullo(l, x, y, x+altura/2, y-altura/2, x+desvX, y-altura);
-    var desv2X = (Math.random() * 10) - 5;
     lineaCapullo(l, x, y, x-altura/2, y-altura/2, x+desv2X, y-altura);
     lineaCapullo(l, x, y, x+altura/2, y-altura/2, x+desv2X, y-altura);
     l.stroke();
 }
 
 function rama(l, xi, yi, nodos){
+    /* Juan Wills animation, see other canvas efects
+    on jcwills.com ⛵ */
     nodos.forEach(n => {
         //l.strokeStyle = 'rgba(58 88 110)'; //quitar
         var pos = {x:xi-n[0], y:yi-n[1]};
@@ -53,8 +56,9 @@ function rama(l, xi, yi, nodos){
     });
 }
 
+var ramas = []
 // ramas que nacen de abajo (orden de abajo hacia arriba)
-rama(ctx, ancho, alto-10, [
+ramas.push([ctx, ancho, alto-10, [
     [30, 65, [
         [45, 20, [
             [20, -5], [15,8]
@@ -67,8 +71,8 @@ rama(ctx, ancho, alto-10, [
             ]
         ]
     ]]
-]);
-rama(ctx, ancho, alto-20, [
+]]);
+ramas.push([ctx, ancho, alto-20, [
     [100, 200, [
         [70, 40, [[20, -10], [23, 30],
             [60, 28, [[10, -5], [11, 15],
@@ -93,17 +97,17 @@ rama(ctx, ancho, alto-20, [
                 ]]
             ]]
         ]]
-]);
-rama(ctx, ancho, alto-30, [
+]]);
+ramas.push([ctx, ancho, alto-30, [
     [90, 230, [[-10, 20],
         [110, 120, [
             [30, 45], [-5, 20]
         ]]
     ]]
-]);
+]]);
 
 // Ramas que nacen del medio hacia arriba
-rama(ct2, ancho, alto-350, [
+ramas.push([ct2, ancho, alto-350, [
     [70, 200, [[50, 20, [
         [30, 2], [25, 25],
     ]], [5, 80, [
@@ -117,11 +121,27 @@ rama(ct2, ancho, alto-350, [
             ]]
         ]]
     ]]
-]);
-rama(ct2, ancho, alto-320, [
+]]);
+ramas.push([ct2, ancho, alto-320, [
     [35, 150, [[40, 25], [5, 65],
         [40, 55, [
             [40, 5], [25, 50]
         ]]
     ]]
-]);
+]]);
+
+ramas.forEach(r => { rama(r[0], r[1], r[2], r[3]) });
+// Animando
+function animar(){
+    /* Juan Wills animation, see other canvas efects
+    on jcwills.com ⛵ */
+    var diff = (new Date().getTime()-ahora)/(1000);
+    console.log(Math.sin(diff));
+    desvX += Math.sin(diff)/50;
+    desv2X -= Math.sin(diff)/50;
+    ctx.clearRect(0,0, ancho, alto);
+    ct2.clearRect(0,0, ancho, alto);
+    ramas.forEach(r => { rama(r[0], r[1], r[2], r[3]) });
+}
+var ahora = new Date().getTime();
+setInterval(animar, 1000/60);
